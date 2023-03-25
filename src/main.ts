5509c201c253b217d9a1cfe912a7a882f7bf09dd
Andrew A <andrewadev@gmail.com>
2023-03-25 10:51:33 +0100

    Add Tailwind
    Add Tailwind CSS along with supporting dependencies (incl. PostCSS).
    
    Minor update to ESLint config to use node env on config files.
    
    Also, needed to manually specify 'content' prop (which files to process).

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import './index.pcss'
 
 const app = createApp(App)
 
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/views/**/*.{vue,js,ts,jsx,tsx,html}', './src/components/**/*.{vue,,js,ts,jsx,tsx,html}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
