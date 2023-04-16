5a2b14fb02372a98f832c6aa98c78f4c938ed69d
Andrew A <andrewadev@gmail.com>
2023-04-16 11:46:58 +0200

    User input is fed to the worksheet
    Use the scrambled user input to feed into the worksheet.
    
    We still use the default sentences to start with, but accept any changes the user might make.
    
    For now there is no global state, so navigation away clears input.

--- src/views/WorksheetView.vue
+++ src/views/WorksheetView.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="border-div">
-    <ScrambledSentence v-for="(sentence, idx) in sentences" :parts="sentence" :key="idx" />
+    <WorksheetForm v-model:sentences="inputSentences" />
+  </div>
+  <div class="border-div">
+    {{ inputSentences }}
+  </div>
+  <div class="border-div">
+    <ScrambledSentence v-for="(sentence, idx) in inputSentences" :parts="sentence" :key="idx" />
   </div>
 </template>
 
 <script setup lang="ts">
+import WorksheetForm from '@/components/forms/WorksheetForm.vue';
 import ScrambledSentence from '@/components/ScrambledSentence.vue';
+import { ref } from 'vue';
 
+// TODO: Move to better location (state default?)
 const sentences = [
   ['The quick brown fox', 'jumps', 'over', 'the lazy dog.'],
   ['The', 'waiter', 'brings', 'us', 'our', 'food.'],
@@ -14,6 +23,8 @@ const sentences = [
   ['The', 'bird', 'flies'],
   ['A', 'hush', 'falls', 'over', 'the', 'crowd.'],
 ];
+
+const inputSentences = ref(sentences);
 </script>
 
 <style scoped>
