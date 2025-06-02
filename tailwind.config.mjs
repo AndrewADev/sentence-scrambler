304db2acb2faebfbcb4c55cc69860f794fbe3386
Andrew A <AndrewADev@gmail.com>
2025-06-02 18:39:40 +0200

    fix(build): Vite CJS warning
    Add missing type specifier to fix a warning from vite. Also convert settings files to ECMAScript Modules.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./src/views/**/*.{vue,js,ts,jsx,tsx,html}', './src/components/**/*.{vue,,js,ts,jsx,tsx,html}'],
   theme: {
     extend: {},
